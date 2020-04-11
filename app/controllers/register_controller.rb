9fbf66f316f73e51f64c04d97c25e0fb5ab9979d
scottolsen <solsen300@gmail.com>
2020-04-11 06:52:25 -0500

    Setup OAuth

---
+++ app/controllers/register_controller.rb
@@ -0,0 +1,27 @@
+class RegisterController < ApplicationController
+  def new
+    redirect_to strava_url
+  end
+
+  def create
+    @response = client.oauth_token(code: params[:code])
+  end
+
+  private
+
+  def client
+    Strava::OAuth::Client.new(
+      client_id: ENV['STRAVA_CLIENT_ID'],
+      client_secret: ENV['STRAVA_SECRET']
+    )
+  end
+
+  def strava_url
+    client.authorize_url(
+      redirect_uri: ENV['STRAVA_REDIRECT_URI']
+      approval_prompt: 'force',
+        response_type: 'code',
+        scope: 'profile:read_all,activity:read_all',
+    )
+  end
+end
